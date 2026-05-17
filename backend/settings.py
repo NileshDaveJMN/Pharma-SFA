f77bfe5bca5560f7e71e8ff7eb0cb33e541ea7f0
Nilesh Dave <nileshdave1511@gmail.com>
2026-05-17 21:00:58 +0530

    Fixed static slash

--- backend/settings.py
+++ backend/settings.py
@@ -125,7 +125,7 @@ REST_FRAMEWORK = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 import os
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
