b262646bbb5919e189d429f1bed1a62f46fdbd9a
Nilesh Dave <nileshdave1511@gmail.com>
2026-07-27 14:19:52 +0530

    exp

--- backend/urls.py
+++ backend/urls.py
@@ -4,12 +4,12 @@ URL configuration for backend project.
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve # 🌟 NAYA IMPORT
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('SFA.urls')),
+    
+    # 🌟 FIX: Explicitly serve media files even when DEBUG=False (For Render testing)
+    path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
-# 🌟 FIX: Ab ye DEBUG=False (Render) par bhi media files serve karega
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
